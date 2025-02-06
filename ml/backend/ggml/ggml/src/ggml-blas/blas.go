94ce94724455ed4f2218a3936a7c9b214ea5dad5
hyg <hyg@webterren.com>
2025-02-07 00:19:04 +0800

    llm: enable metal backend on macOS amd64
    ref: 1. https://github.com/ollama/ollama/commit/cb85daa89e28131447fb738958706391a5ad57a3

--- ml/backend/ggml/ggml/src/ggml-blas/blas.go
+++ ml/backend/ggml/ggml/src/ggml-blas/blas.go
@@ -1,10 +1,10 @@
-//go:build darwin && arm64
+//go:build darwin
 
 package blas
 
 // #cgo CXXFLAGS: -std=c++11
 // #cgo CPPFLAGS: -DGGML_USE_BLAS
 // #cgo CPPFLAGS: -I${SRCDIR}/.. -I${SRCDIR}/../../include
-// #cgo darwin,arm64 CPPFLAGS: -DGGML_BLAS_USE_ACCELERATE -DACCELERATE_NEW_LAPACK -DACCELERATE_LAPACK_ILP64
-// #cgo darwin,arm64 LDFLAGS: -framework Accelerate
+// #cgo darwin CPPFLAGS: -DGGML_BLAS_USE_ACCELERATE -DACCELERATE_NEW_LAPACK -DACCELERATE_LAPACK_ILP64
+// #cgo darwin LDFLAGS: -framework Accelerate
 import "C"
